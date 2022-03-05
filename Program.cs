b66a500a76e4904901a0e2184e519164d1b419a8
amorimlt <julianeamorim.lt@gmail.com>
2022-03-04 22:21:21 -0300

    finaliza exercicío

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("FOGO!");
+Console.ResetColor();
